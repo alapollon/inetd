123db5c5668dd6967006179b7276dc700ceb551f
Wilfred Achila Alapollon <dev@achila.me>
2025-09-10 04:20:47 -0500

    backlogging model up to date

---
+++ model.py
@@ -0,0 +1,318 @@
+from sqlalchemy.orm import attribute_keyed_dict, collections, column_property, DeclarativeBase, Mapped, MappedAsDataclass, mapped_column, relationship
+from sqlalchemy import create_engine, Column, Integer, ForeignKey, func, MetaData, String, select, Table
+import ip, uuid, time, typing
+
+
+
+
+def __init__(self, args):
+    self.engine= create_engine(args.get(["api"], False), echo=args.get['echo'], logging_name="scylladb", enable_from_linting=args.get['lint'], =ars.get['isolation'],  ) 
+    self.session=Session
+    self.meta_data=MetaData( bind= self.engine )
+    self.date=time
+
+
+class Base(DeclarativeBase):
+    pass 
+
+class PrimaryTableSchema(MappedAsDataclass, Base, unsafe_hash=True ):
+    __tablename__ = "mac_table__schema"
+    index=Column(Integer, unique=True, nulable=False)
+    universal:Mapped[String]=mapped_column("no", String, primary_key=True)
+    vendor=Column("hardware", String)
+    last2date: Mapped[datetime]=mapped_column("update", insert_default=func.est_timestamp(), default=None)
+    macaddr: Mapped[String]=mapped_column("macaddress", String, primary_key=True)
+
+    @property
+    def nic_key(self):
+        return (self.universal , self.macaddr )
+
+    def __init__(self, **device, lastupdate):
+        self.universal=device.get(["uuid"], universal_receipts())
+        self.lastupdate=lastupdate
+        self.vendor=
+        self.macaddr= 
+
+
+class GatewaySchema( Base):
+    __tablename__= "main_gateway__scheme"
+    index=Column(unique=True, nullable=False)
+    universal =mapped_column("no", String , unique=True, nullable=False)
+    cidr= Column("hostmask", Integer )
+    zone=Column(String, primary_key=True )
+    endpoints=column_property("ends", Integer, endpoint_count=select(func.count(EdgeTableSchema.gateway).where(EdgeTableSchema.gateway == universal ).scalar_subquery())) 
+    hops=Column( Integer, primary_key=True  )
+    os=Column("interface", String ) 
+    macaddr=mapped_column("nic", String, unique=True )
+    ip=Column("inet", String)
+    longip= Column("inet6", unique= True, primary_key=True, nullable=False) 
+    dname=Column("domain", primary_key=True )
+
+    
+    spine=relationship(
+        "NodeRelationships",
+        secondary=Table(
+            "NodeSchema",
+            self.meta_data,
+            Column("layer", String ),
+            Column("proxy", Boolean ) 
+
+        ),
+        backref="core"
+    )
+    edge: Mapped[Dict[str, "EdgeRelationship"]]=relationship(
+        collection_class=attribute_keyed_dict("target")
+        , cascade="all, delete-orphan",
+        backref="spanningtree"
+    )
+
+    @property
+    def node_count(self):
+        stmt= select(func.count(NodeSchema)).where(NodeSchema.gateway).scalar_subquery()
+        return stmt
+    
+    def access(self):
+        pass 
+
+    def __init__(self, **gateway):
+        self.inet6=addr.get["ipv6"]
+        self.inet=
+        self.os==addr.get(["os"])
+        self.dname=addr.get["domain"]
+
+
+class NodeSchema( Base):
+    __tablename__= "node_edge_scheme"
+    index=Column(Integer)
+    universalid=Column(String, primary_key=True  )
+    os=Column("interface", String, primary_key=True)
+    layer=Column("model", String, primary_key=True)
+    hops=Column(Integer)
+    cidr=Column(Integer, primary_key=True, nullable=False)
+    bgp=Column(Boolean)
+    gateway=Column( String, primary_key=True) 
+    macaddr=Column( String, primary_key=True )
+    ip=Column(String, nullable=False)
+    longip=Column(String, nullable=True)
+    ForeignKeyConstraint( ["universalid", "vendor",""],["PrimaryTableSchema.universalid","PrimaryTableSchema.macaddr"])
+    borders=relationship(
+        "",
+        seconday=Table(
+            "",
+            self.meta,
+            Column("node_gateway", String, ForeignKey(""), primary_key=True ),
+            Column("gateway_zone", String, ForeignKey(""), primary_key=True),
+            Column("hostcount", String, ForeignKey("")),
+            Column("edge_uuid", String, ForeignKey())
+        ),
+        backref="nodes"
+    )
+
+
+    
+    def __init__(self, **node):
+        self.universal=
+        self.gateway=
+        self.macaddr=
+        self.hops=
+
+class HostTable(Functionality):
+    __tablename__="hosts"
+    index=Column(Integer, unique=True)
+    universal=Column(String, unique=True, primary_key=True)
+    hops=Column("hops", Integer, )
+    os=Column("os", String )
+    timezone=Column(String)
+    macaddr=Column(String, unique=True, primary_key=True)
+    longip=Column("inet6")
+    ip=Column("inet")
+    gateway=Column("access", String, primary_key=True)
+    ForeignKeyConstraint( ["universalid", "gateway"], ["PrimaryTableSchema.macaddr","PrimaryTableSchema.universalid"])
+    neighbor=relationship(
+        "LeafTable",
+        secondary=Table(
+            "",
+            self.meta,
+            Column(),
+            Column()
+        ),
+        backref="neighbor"
+    )
+    fitness=Column()
+
+    def __init__(self, **host):
+        self.mac=
+        self.os=
+        self.tz=
+        self.fitness= 
+        pass
+
+
+
+class KansasCinncinatiSchema( Base):
+    __tablename__="kansas_cincinnati_scheme"
+    index=Column(Integer)
+    universal=mapped_column(unique=True, primary_key=True, nullable=True)
+    hops=Column( Integer nullable=False)
+    targetmac=mapped_column(String, unique=True, nullable=False)
+    targetip=Column( String, nullable=False )
+    targetlongip=Column( String, unique=True )
+    gateway=Column(String, nullable= False ) 
+    ForeignKeyConstraint(
+        ["universalid","targetmac"],["PrimaryTableSchema.uuid","PrimaryTableSchema.macaddr"]
+    )
+    fitness=()
+
+    edges=relationship(
+        "onpath",
+        secondary=Table(
+            "EdgeTableSchema",
+            self.meta,
+            Column("", ForeignKey("")),
+            Column("target", ),
+            Column("")
+        ),
+        backref="route"
+    )
+
+    def __init__(self, **target):
+        self.mac=
+        self.gatewayaddress=
+        self.targetinet= 
+        self.targetinet6=
+        
+        pass
+
+
+
+
+class EdgeRelationship( Base):
+    __tablename__="route_schemes"
+    index=Column(Integer)
+    universal=column_property()
+    hops=Column( Integer  )
+    latency=Column( Float)
+    jitter=Column( Float )
+    tz=Column( String )
+    target=column_property()
+
+    neighbor=relationship(
+        "HostTable",
+
+    
+    )
+    gateway=relationship(
+        "HostTable",
+        mapped_column(String, )
+        lazy=selectin 
+
+    )
+    sssp=column_property()
+
+    def _hostmask_address(self, gateway):
+        return self.
+
+    ForeignKey(["universal","destination"],["PrimaryTableSchema.universal","PrimaryTableSchema.macaddr"])
+    def __init__(self, **stats):
+        self.hops=
+        self.latency=
+        self.jitter=
+        self.hostcount=
+        self.nodecount=
+        self.timezone=
+
+class DatabaseTableSchema( Base):
+    __tablename__="database_routes_schema"
+    index=Column(Integer)
+    universal=Column(String, primary_key=True, nullable=False )
+    cname=Column(String, nullable=False )
+    hostname=Column(String)
+    ip=Column(String, primary_key=True )
+    longip=mapped_column(String, primary_key=True )
+    macaddr=Column(String, primary_key=True, nullable=False )
+    port=Column(Integer)
+    ForeignKeyConstraint(["universal","macaddr"],["PrimaryTableSchema.universal", "PrimaryTableSchema.macaddr", "PrimaryTableSchema.lastupdate"])
+    fitness=Column
+    domain=relationship(
+        "Database",
+        Base.metadata,
+        self.meta,
+        Column("gateway", String, ),
+        Column("host", String, ),
+        Column("vendor", String )
+    )
+
+    def __init__(self, **database):
+        self.port=
+        self.internet=
+        self.hostname=
+        self.cname=
+        self.fitness= 
+
+
+gateway=GatewaySchema.alias("gateway")
+node=NodeSchema.alias("node")
+host=HostSchema.alias("host")
+kansas_cincinnati_scheme=KansasCinncinatiSchema.alias("kansas_cincinnati_scheme")
+primary=PrimaryTableSchema.alias("primary")
+
+def __create__(*tables):
+    pass 
+
+def push(func):
+    try:
+        with self.session as session:
+            async def __update__(*args, **index):
+                for key, record in enumerate(index):
+                    while table in record:
+                        if table == "primary":
+                            select().join([...record["kpi"]]).where(primary.macaddr == record['primary']).where()
+                            pass
+                        if table == "host":
+                            select().join([...record["kpi"]]).where(host.macaddr == record['host']).where()
+                            pass
+                        if table == "node":
+                            select().join([...record["kpi"]]).where(node.gateway == record['gateway']).where()
+                            pass 
+                        if table == "gateway":
+                            select(gateway).join([...record["kpi"]).where(gateway.cidr == record.get('cidr', False) | gateway.macaddr == record.get('mac')).where( )
+                            pass
+                            
+                pass
+            async def __insert__(**data):
+                for key, record in enumerate(data):
+                    while table in record:
+                        a= record['ip']['inetp6'] if Not record.get("macaddress", False) 
+                        index= select(table).where(table.longip == a) if 
+                        ``
+                pass 
+            if isinstance():
+                return __update__(func)
+            elif isinstance():
+                return __insert__(func)
+    except:
+
+def pull(func):
+    try:
+        def __query__(**data):
+            for key, record in enumerate(data):
+                while table in record:
+                    kpi= select(table).where(table.mac.in_([...record[]))
+                    for index in session.scalar(kpi):
+                        return index
+
+        async def __search__(**query):
+            keys=list(query.keys())
+            for key, record in query:
+                while vector in keys:
+                    for data in record['quest']:
+                        kpi = select(record['sample']).where(...record.get())
+                        return kpi
+            pass 
+        if isinstance():
+            return __query__(func)
+        else:
+            return __search__(func)
+    except:
+
+
